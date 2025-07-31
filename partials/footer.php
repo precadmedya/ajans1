b3012ade9077c6d1cef23d53fe0c48d67062ddca
precadmedya <precadmedia@gmail.com>
2025-07-31 13:18:30 +0300

    Add agency login and registration system

---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+</div>
+<footer class="py-3 my-4 border-top text-center">
+    <p class="text-muted">&copy; 2024 Yazılım Ustası</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
